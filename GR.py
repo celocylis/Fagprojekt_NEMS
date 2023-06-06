89ffb008a9ed40f07e8df91b54e4d9fb41c094a7
NoahEmilLaursen <124694548+NoahEmilLaursen@users.noreply.github.com>
2023-06-06 10:04:46 +0200

    Add files via upload

--- GR.py
+++ GR.py
@@ -1,63 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 14 14:55:32 2023
+Created on Mon Jun  5 16:43:01 2023
 
 @author: noahe
 """
-
-import xarray as xr
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-
-# load data:
-ds = xr.open_dataset('C:/Users/noahe/OneDrive/Dokumenter/DTU/Fagprojekt/NEMS_colocated/Nimbus5-NEMS_L2_1972m1217t011144_DR24_era5.nc')
-#ds = xr.open_dataset('C:/Users/noahe/OneDrive/Dokumenter/DTU/Fagprojekt/NEMS_colocated/Nimbus5-NEMS_L2_1973m0617t031041_DR26_era5.nc')
+import numpy as np
+import pickle
 
 
+#with open('C:/Users/noahe/OneDrive/Dokumenter/DTU/Fagprojekt/RTMfcomisoDec.pkl','rb') as f:
+with open('C:/Users/noahe/OneDrive/Dokumenter/DTU/Fagprojekt/RTMfcomisoJun.pkl','rb') as f:
+    ds = pickle.load(f)
 
-colors_list = []
+data = np.array(ds)
 
-for i in range(14):
-    colors = []
-    GR = (ds.TBNEMS[:,i,1]-ds.TBNEMS[:,i,0])/(ds.TBNEMS[:,i,0]+ds.TBNEMS[:,i,1]) 
-    
-    for value in GR:
-        if value > 0.01:
-            colors.append('blue')
-        elif value <= -0.1:
-            colors.append('black')
-        else:
-            colors.append('green')
-    colors_list.append(colors)
-    
-plt.figure(1)
-for i in range(16):
-    globals()[f'LAT{i}'] = ds.LAT[:, i]
-    globals()[f'LON{i}'] = ds.LON[:, i]
+T31 = data[:,3]
+T22 = data[:,4]
+lat =data[:,0]
+lon = data[:,1]
 
-#North pole
-ax = plt.axes(projection=ccrs.NorthPolarStereo())
-ax.set_extent([-180, 180, 90, 60], ccrs.PlateCarree())
-
-ax.coastlines()
-
-for i in range(14):
-    plt.scatter(globals()[f'LON{i}'], globals()[f'LAT{i}'], s=0.1, c=colors_list[i],transform=ccrs.PlateCarree())
-plt.show(1)
-
-plt.figure(2)
-for i in range(16):
-    globals()[f'LAT{i}'] = ds.LAT[:, i]
-    globals()[f'LON{i}'] = ds.LON[:, i]
-
-#South pole
-ax = plt.axes(projection=ccrs.SouthPolarStereo())
-ax.set_extent([-180, 180, -90, -60], ccrs.PlateCarree())
-
-ax.coastlines()
-
-for i in range(14):
-    plt.scatter(globals()[f'LON{i}'], globals()[f'LAT{i}'], s=0.1, c=colors_list[i],transform=ccrs.PlateCarree())
+nyGR = []
 
 
-plt.show(2)
+for x in range (len(data)):
+    GR = (T31[x]-T22[x])/(T31[x]+T22[x])
+    nyGR.append([lat[x],lon[x],GR])
+
+with open('GRJun.pkl', 'wb') as f:
+    pickle.dump(nyGR, f)
+
